8b79c2a17b5cb74b0b13cdc7b53edc560e73f399
Mustafa Furkan Bulut <51883559+mfbulut@users.noreply.github.com>
2022-05-04 00:53:38 +0300

    Formatting

--- main.c
+++ main.c
@@ -1,35 +1,33 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 int main() {
-    int a, b, c, d;
-    a = b = c = d = 0;
+  int a, b, c, d;
+  a = b = c = d = 0;
 
-    for (int i = 0; i < 100000; i++)
-    {
-        while ((a + 1) * (a + 1) <= i) {
-            ++a;
-        }
-
-        b = a;
-        c = i - a * a;
-
-        if (a) {
-            d = c / a;
-            b += d;
-            c -= d * a;
-
-            while (c)
-            {
-                --a;
-                c += b;
-                d = c / a;
-                b += d;
-                c -= d * a;
-            }
-        }
-
-        printf("Number: %d\n", i);
-        printf("First Factor: %d\n", a);
-        printf("Second Factor: %d\n\n", b);
+  for (int i = 0; i < 100000; i++) {
+    while ((a + 1) * (a + 1) <= i) {
+      ++a;
     }
+
+    b = a;
+    c = i - a * a;
+
+    if (a) {
+      d = c / a;
+      b += d;
+      c -= d * a;
+
+      while (c) {
+        --a;
+        c += b;
+        d = c / a;
+        b += d;
+        c -= d * a;
+      }
+    }
+
+    printf("Number: %d\n", i);
+    printf("First Factor: %d\n", a);
+    printf("Second Factor: %d\n\n", b);
+  }
 }
